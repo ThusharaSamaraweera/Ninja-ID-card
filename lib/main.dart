0bf3ed622fe01fa12514dc69ddbd33f9149fc5a8
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-03-09 11:34:51 +0530

    add header

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,12 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('appbar'),
+        title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
     );
   }
